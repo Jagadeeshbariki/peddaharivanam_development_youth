e2f096e1f01580616d7d4d6847a9be89ccbd59fb
jagadeesh <vyomanuatjagadeesh@gmail.com>
2024-10-20 18:39:25 +0530

    Adding the facicons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Menu from './Components/Menu/Menu';
+import 'bootstrap/dist/css/bootstrap.css';
+import About from './Components/About/About';
+
 function App() {
   return (
     <div className="App">
       <Header/>
       <Menu/>
       <Home/>
+      <About/>
     </div>
   );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Home.module.css';
 import DevImg from '../../Assets/ImgDev.png';
+import YouthImg from '../../Assets/YouthImage.png';
 
 const Home = () => {
   return (
@@ -19,9 +20,22 @@ const Home = () => {
                 <li>Digital Literacy and E-Governance</li>
                 <li>Emergency Relief and Disaster Management</li>
             </ol>
+            <a href='https://wa.me/916300963789?text=I%20am%20interested%20to%20Join%20Your%20Group' target="_blank" rel="noreferrer"><button className={`${styles.JoinBtn}`}>Join Us</button></a>
         </div>
         <div className={`${styles.ImgDevOfVlg}`}>
             <img className={`${styles.devimg}`} src={DevImg} alt='VillageImg' />
+        </div>
+      </div>
+      <h2 className={`${styles.HomeSecondSectionHeading}`}>Pedda Harivanam Development Youth: Building a Brighter Future Together</h2>
+      <div className={`${styles.HomeSecondSection}`}>
+        <div className={`${styles.OurIntro}`}>
+          
+          <p><em><b>"Pedda Harivanam Development Youth"</b></em> is a non-political group of passionate youth dedicated to the betterment of our village. Our mission is to address local issues, foster growth, and ensure transparency by holding local leaders, panchayati, and sachivalayam employees accountable for village development. We will actively engage in activities that improve infrastructure, promote cleanliness, and bring modern facilities to the village, such as public services and health initiatives. By uniting, we aim to build a brighter future for Pedda Harivanam, empowering the next generation and ensuring that every voice in our village is heard.</p>
+
+          <p><b>Let’s work together for a cleaner, healthier, and more prosperous village!</b></p>
+        </div>
+        <div className={`${styles.secondSectionImg}`}>
+         <img src={YouthImg} alt='Youth Discussion' />
         </div>
       </div>
     </div>
