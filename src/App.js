ce483c3a1f60ef9eefa65582ccc7b1b4ba014127
jagadeesh <vyomanuatjagadeesh@gmail.com>
2024-10-22 11:27:52 +0530

    Adding the facicons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Menu from './Components/Menu/Menu';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter  as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename="/peddaharivanam_development_youth/">
+    <Router>
       <div className="App">
         <Header/>
         <Menu/>
